5d1b5acda8f2db6c6c33f022302e77544c5bb7cc
Your Name <you@example.com>
2023-03-22 05:09:04 -0700

    simple project to begin learning Elixir tricks

---
+++ dictionary/lib/dictionary.ex
@@ -0,0 +1,19 @@
+defmodule Dictionary do
+  @moduledoc """
+  Documentation for `Dictionary`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Dictionary.hello()
+      :world
+
+  """
+  
+  def hello do
+    IO.puts "Hello World!"
+  end
+end
