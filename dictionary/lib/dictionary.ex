71c65ba8cd9cafddbe21dac1dd50947845e24392
Your Name <you@example.com>
2023-04-07 15:07:36 -0700

    Updates to make it clear, functional, and idiomatic

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -12,8 +12,23 @@ defmodule Dictionary do
       :world
 
   """
-  
   def hello do
     IO.puts "Hello World!"
+  end
+
+  def word_list do
+    "assets/words.txt"
+    |> File.read!()
+    String.split(words, ~r/\n/, trim: true)
+  end
+
+  def random_word do
+    Enum.random(word_list())
+
+  end
+
+  def random_phrase do
+    random_phrase = "had we but world enough, and time"
+    String.split(random_phrase)
   end
 end
